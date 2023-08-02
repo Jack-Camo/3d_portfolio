c65e4392f87bc8b79bcc8f1cb5ffb3008778fd2e
Jack-Camo <118351840+Jack-Camo@users.noreply.github.com>
2023-08-02 21:40:41 +0100

    + Changed loader from div to mesh to fix three.js error + frameloop naming changed to fix error

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -21,7 +21,7 @@ const Computers = () => {
 const ComputersCanvas = () => {
   return (
     <Canvas
-      frameLoop="demand"
+      frameloop="demand"
       shadows
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{preserveDrawingBuffer: true}}
@@ -31,10 +31,9 @@ const ComputersCanvas = () => {
           enableZoom={false} 
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
-        /> {/* Allows for spinning the model*/}
+        />
         <Computers />
       </Suspense>
-
       <Preload all />
     </Canvas>
   )
