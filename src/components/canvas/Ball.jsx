f29255e817810c312930dfa4dc292748b4aea462
Jack-Camo <118351840+Jack-Camo@users.noreply.github.com>
2023-08-12 19:52:14 +0100

    + Completed Works.jsx component + Minor changes to ball + Corrected spelling mistake in css class name in SectionWrapper.jsx

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -10,7 +10,7 @@ const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
 
   return (
-    <Float speed={4} rotationIntensity={1} floatIntensity={2}>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={1} />
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
@@ -24,6 +24,7 @@ const Ball = (props) => {
         <Decal 
           position={[0, 0, 1]}
           rotation={[2 * Math.PI, 0, 6.25]}
+          scale={1}
           flatShading
           map={decal}
         />
@@ -36,14 +37,14 @@ const BallCanvas = ({ icon }) => {
   return (
     <Canvas
       frameloop="demand"
+      dpr={[1, 2]}
       gl={{preserveDrawingBuffer: true}}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          enableZoom={false} 
-        />
+        <OrbitControls enableZoom={false}/>
         <Ball imgUrl={icon} />
       </Suspense>
+
       <Preload all />
     </Canvas>
   )
